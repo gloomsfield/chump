514b1824042652d6ec1d74b24c63e8bb1de383f6
Gloomsfield <amory@acosta.boo>
2026-09-14 15:33:48 -0700

    write status scaffolding

--- src/chump.c
+++ src/chump.c
@@ -1,13 +1,20 @@
 #include "chump.h"
 
-#include <stdint.h>
-#include <stdio.h>
+#define CHUMP_STATUS(CHOOSE_FUNC) \
+	CHOOSE_FUNC(SUCCESS = CHUMP_SUCCESS, "chump - success") \
+	CHOOSE_FUNC(FAILURE = CHUMP_FAILURE, "chump - generic failure") \
 
-struct chump_t {
-	uint32_t test;
+#define CHUMP_CHOOSE_ENUM(ENUM, STRING) ENUM,
+#define CHUMP_CHOOSE_STRING(ENUM, STRING) STRING,
+
+enum chump_status_t {
+	CHUMP_STATUS(CHUMP_CHOOSE_ENUM)
 };
 
-void example_function() {
+const char* status_strings[] = { CHUMP_STATUS(CHUMP_CHOOSE_STRING) };
 
-	printf("waow! -chump");
+const char* chump_get_status_string(chump_status_t status) {
+	return status_strings[status];
 }
+
+
--- src/chump.h
+++ src/chump.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define CHUMP_FAILURE (0)
+#define CHUMP_SUCCESS (1)
+
 typedef struct chump_t chump_t;
 
-void example_function();
+typedef enum chump_status_t chump_status_t;
+
+const char* chump_get_status_string(chump_status_t status);
